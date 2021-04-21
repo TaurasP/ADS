da1905b6d73789bf3f487df21cfee4951fbeaa4b
Tauras <taurasp1@gmail.com>
2021-04-21 13:09:45 +0300

    finished 1st task (TIESINE IR DVEJETAINE PAIESKA)

---
+++ src/Car.java
@@ -0,0 +1,167 @@
+import java.util.*;
+
+public class Car {
+    private String manufacturer;
+    private String model;
+    private int year;
+    private String color;
+    private int kilowatts;
+    private String fuelType;
+    private String transmissionType;
+    private String driveWheels;
+    private double fuelTankCapacity;
+
+    public Car(String manufacturer, String model, int year, String color, int kilowatts, String fuelType,
+               String transmissionType, String driveWheels, double fuelTankCapacity) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.kilowatts = kilowatts;
+        this.fuelType = fuelType;
+        this.transmissionType = transmissionType;
+        this.driveWheels = driveWheels;
+        this.fuelTankCapacity = fuelTankCapacity;
+    }
+
+    public Car(String manufacturer, String model) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getKilowatts() {
+        return kilowatts;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public String getTransmissionType() {
+        return transmissionType;
+    }
+
+    public String getDriveWheels() {
+        return driveWheels;
+    }
+
+    public double getFuelTankCapacity() {
+        return fuelTankCapacity;
+    }
+
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static int[] scanNumbers() {
+        int num, numTotal;
+        int[] numArray;
+
+        System.out.print("Iveskite norima ivesti skaiciu kieki: ");
+        numTotal = scanner.nextInt();
+        numArray = new int[numTotal];
+
+        for (int i = 0; i < numArray.length; i++) {
+            num = scanner.nextInt();
+            numArray[i] = num;
+        }
+        return numArray;
+    }
+
+    public static void printNumbers(int[] arr) {
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static int scanNumber() {
+        int key;
+
+        System.out.print("Iveskite ieskoma skaiciu: ");
+        key = scanner.nextInt();
+
+        return key;
+    }
+
+    public static String scanText() {
+        String key;
+
+        System.out.print("Iveskite ieskoma zodi: ");
+        key = scanner.nextLine();
+
+        return key;
+    }
+
+    public static void linearNumbersSearch() {
+        int key;
+        int[] arr;
+
+        arr = scanNumbers();
+        Arrays.sort(arr);
+        printNumbers(arr);
+        key = scanNumber();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                System.out.println("Skaiciaus " + key + " indeksas: " + i + ".");
+                return;
+            }
+        }
+        System.out.println("Toks skaicius nerastas!");
+    }
+
+    public static void linearCarsSearch(List<Car> cars) {
+        String key;
+
+        key = scanText();
+
+        for (int i = 0; i < cars.size(); i++) {
+            if (cars.get(i).getManufacturer().equalsIgnoreCase(key) || cars.get(i).getModel().equalsIgnoreCase(key)) {
+                System.out.println("Rastas automobilis " + cars.get(i).getManufacturer() + " " + cars.get(i).getModel() + ", indeksas: " + i + ".");
+                return;
+            }
+        }
+        System.out.println("Toks elementas nerastas!");
+    }
+
+    public static void binaryNumbersSearch() {
+        int first, mid, last, key;
+        int[] arr;
+
+        arr = scanNumbers();
+        Arrays.sort(arr);
+        printNumbers(arr);
+        key = scanNumber();
+        first = 0;
+        last = arr.length - 1;
+        mid = (first + last) / 2;
+
+        while( first <= last ){
+            if ( arr[mid] < key ){
+                first = mid + 1;
+            } else if ( arr[mid] == key ){
+                System.out.println("Skaiciaus " + key + " indeksas: " + mid + ".");
+                break;
+            } else {
+                last = mid - 1;
+            }
+            mid = (first + last) / 2;
+        }
+        if ( first > last ){
+            System.out.println("Toks skaicius nerastas!");
+        }
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
@@ -6,35 +6,55 @@ public class Main {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws InterruptedException {
-        startProgram();
+        startProgram(createCars());
+    }
+
+    public static List<Car> createCars() {
+        List<Car> cars = new ArrayList<>();
+
+        Car mazdaCX5 = new Car("Mazda", "CX-5");
+        Car mazda5 = new Car("Mazda", "5");
+        Car bmwX5 = new Car("BMW", "X5");
+
+        cars.add(mazdaCX5);
+        cars.add(mazda5);
+        cars.add(bmwX5);
+
+        return cars;
     }
 
     public static void printMenu() {
         System.out.println("\n---------------------------MENIU---------------------------");
         System.out.println("---------------TIESINE IR DVEJETAINE PAIESKA---------------");
-        System.out.println("1. Tiesine paieska.");
-        System.out.println("2. Dvejetaine paieska.");
+        System.out.println("1. Tiesine paieska pagal raktini zodi.");
+        System.out.println("2. Tiesine paieska pagal raktini zodi is automobilio objekto.");
+        System.out.println("3. Dvejetaine paieska pagal raktini zodi.");
+
         System.out.println("-------------------RUSIAVIMO ALGORITMAI--------------------");
-        System.out.println("3. Skaiciai.");
-        System.out.println("4. Simboliai.");
-        System.out.println("5. Tekstas.");
+        System.out.println("4. Skaiciai.");
+        System.out.println("5. Simboliai.");
+        System.out.println("6. Tekstas.");
+
         System.out.println("---------------TIESINES DUOMENU STRUKTUROS-----------------");
-        System.out.println("6. Stekas.");
-        System.out.println("7. Eile.");
-        System.out.println("8. Sarasas.");
-        System.out.println("9. Vektorius.");
+        System.out.println("7. Stekas.");
+        System.out.println("8. Eile.");
+        System.out.println("9. Sarasas.");
+        System.out.println("10. Vektorius.");
+
         System.out.println("-------------COMPOSITE PROJEKTAVIMO SABLONAS---------------");
-        System.out.println("10. Idejimas.");
-        System.out.println("11. Atvaizdavimas.");
+        System.out.println("11. Idejimas.");
+        System.out.println("12. Atvaizdavimas.");
+
         System.out.println("-----------------------------------------------------------");
         System.out.println("0. Iseiti is programos.");
         System.out.println("-----------------------------------------------------------");
+
         System.out.print("Pasirinkite norima punkta:");
     }
 
 
 
-        public static void startProgram() throws InterruptedException {
+        public static void startProgram(List<Car> cars) throws InterruptedException {
 
         boolean quit = false;
         int choice;
@@ -46,55 +66,61 @@ public class Main {
 
             switch (choice) {
                 case 1:
-                    Search.linearSearch();
+                    Car.linearNumbersSearch();
                     TimeUnit.SECONDS.sleep(3);
                     printMenu();
                     break;
                 case 2:
-                    Search.binarySearch();
+                    //System.out.println("\nTIESINE IR DVEJETAINE PAIESKA - Tiesine paieska pagal raktini zodi is automobilio objekto.");
+                    Car.linearCarsSearch(cars);
                     TimeUnit.SECONDS.sleep(3);
                     printMenu();
                     break;
                 case 3:
+                    Car.binaryNumbersSearch();
+                    TimeUnit.SECONDS.sleep(3);
+                    printMenu();
+                    break;
+                case 4:
                     System.out.println("\nRUSIAVIMO ALGORITMAI - Skaiciai");
                     /*TimeUnit.SECONDS.sleep(3);
                     printMenu();*/
                     break;
-                case 4:
+                case 5:
                     System.out.println("\nRUSIAVIMO ALGORITMAI - Simboliai");
                     /*TimeUnit.SECONDS.sleep(3);
                     printMenu();*/
-                case 5:
+                case 6:
                     System.out.println("\nRUSIAVIMO ALGORITMAI - Tekstas");
                     /*TimeUnit.SECONDS.sleep(3);
                     printMenu();*/
                     break;
-                case 6:
+                case 7:
                     System.out.println("\nTIESINES DUOMENU STRUKTUROS - Stekas");
                     /*TimeUnit.SECONDS.sleep(3);
                     printMenu();*/
                     break;
-                case 7:
+                case 8:
                     System.out.println("\nTIESINES DUOMENU STRUKTUROS - Eile");
                     /*TimeUnit.SECONDS.sleep(3);
                     printMenu();*/
                     break;
-                case 8:
+                case 9:
                     System.out.println("\nTIESINES DUOMENU STRUKTUROS - Sarasas");
                     /*TimeUnit.SECONDS.sleep(3);
                     printMenu();*/
                     break;
-                case 9:
+                case 10:
                     System.out.println("\nTIESINES DUOMENU STRUKTUROS - Vektorius");
                     /*TimeUnit.SECONDS.sleep(3);
                     printMenu();*/
                     break;
-                case 10:
+                case 11:
                     System.out.println("\nCOMPOSITE PROJEKTAVIMO SABLONAS - Idejimas");
                     /*TimeUnit.SECONDS.sleep(3);
                     printMenu();*/
                     break;
-                case 11:
+                case 12:
                     System.out.println("\nCOMPOSITE PROJEKTAVIMO SABLONAS - Atvaizdavimas");
                     /*TimeUnit.SECONDS.sleep(3);
                     printMenu();*/
